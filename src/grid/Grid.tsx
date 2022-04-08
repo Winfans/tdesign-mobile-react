1d726a45e532aef7eccd063b7dae75ed34db0658
Winfans <1123642601@qq.com>
2022-04-08 10:10:43 +0800

    grid组件引入defaultProps、withNativeProps

--- src/grid/Grid.tsx
+++ src/grid/Grid.tsx
@@ -1,24 +1,45 @@
 import React, { FC } from "react";
+import withNativeProps, {
+  NativeProps,
+} from "tdesign-mobile-react/_util/withNativeProps";
 import { TdGridProps } from "./type";
-import useConfig from '../_util/useConfig';
+import useConfig from "../_util/useConfig";
 
-const Grid: FC<TdGridProps> = (prop) => {
-    const { children, align, border, column, gutter } = prop;
+export interface GridProps extends TdGridProps, NativeProps {}
 
-    const { classPrefix } = useConfig();
-    const name = `${classPrefix}-grid`;
-
-    return <>
-        <div className={name} style={{paddingRight: gutter}}>
-            {
-                React.Children.map(children, (child: React.ReactElement) => React.cloneElement(child, {
-                    border,
-                    align,
-                    column,
-                }))
-            }
-        </div>
-    </>
+export enum GridAlignEnum {
+  CENTER = "center",
+  LEFT = "left",
 }
+
+const defaultProps = {
+  gutter: 20,
+  align: GridAlignEnum.CENTER,
+  border: false,
+  column: 4,
+};
+
+const Grid: FC<GridProps> = (props) => {
+  const { children, align, border, column, gutter } = props;
+
+  const { classPrefix } = useConfig();
+  const name = `${classPrefix}-grid`;
+
+  return withNativeProps(
+    props,
+    <div className={name} style={{ paddingRight: gutter }}>
+      {React.Children.map(children, (child: React.ReactElement) =>
+        React.cloneElement(child, {
+          border,
+          align,
+          column,
+        })
+      )}
+    </div>
+  );
+};
+
+Grid.defaultProps = defaultProps;
+Grid.displayName = "gutter";
 
 export default Grid;
--- src/grid/GridItem.tsx
+++ src/grid/GridItem.tsx
@@ -1,145 +1,152 @@
 import React, { FC, useMemo } from "react";
-import { Badge } from 'tdesign-mobile-react'
+import { Badge } from "tdesign-mobile-react";
+import withNativeProps, {
+  NativeProps,
+} from "tdesign-mobile-react/_util/withNativeProps";
 import { TdGridItemProps, TdGridProps } from "./type";
-import useConfig from '../_util/useConfig';
+import useConfig from "../_util/useConfig";
+import { GridAlignEnum } from "./Grid";
 
-enum LAYOUT {
-    VERTICAL = 'vertical',
-    HORIZONTAL = 'horizontal'
+export interface GridItemProp extends TdGridItemProps, TdGridProps, NativeProps {}
+
+export enum GridItemLayoutEnum {
+  VERTICAL = "vertical",
+  HORIZONTAL = "horizontal",
 }
-
-enum ALIGN {
-    CENTER = 'center',
-    LEFT = 'left',
-}
-
-const DEFAULT_COLUMN = 4; 
-
-const DEFAULT_ALIGN = ALIGN.CENTER;
-
-const DEFAULT_GUTTER = 20;
-
-const DEFAULT_LAYOUT = LAYOUT.VERTICAL;
-
-const DEFAULT_BORDER = false;
 
 const getGridItemWidth = (column) => `${100 / column}%`;
 
 const getBorderStyle = (border) => {
-    if (typeof border === 'boolean') {
-        if (border) {
-            return {
-                borderColor: '#f6f6f6',
-                borderWidth: '1px',
-                borderStyle: 'solid'
-            };
-        } 
-        
-        return {}
+  if (typeof border === "boolean") {
+    if (border) {
+      return {
+        borderColor: "#f6f6f6",
+        borderWidth: "1px",
+        borderStyle: "solid",
+      };
     }
-    const {color, width, style} = border;
-    return {
-        borderColor: color || '#f6f6f6',
-        borderWidth: width || '1px',
-        borderStyle: style || 'solid',
-    };
-}
 
-const getLayout = (layout) => {
-    const layoutMap = {
-        [LAYOUT.VERTICAL] : 'column',
-        [LAYOUT.HORIZONTAL]: 'row',
-    }
-    return layoutMap[layout];
-}
-
-const getGridItemTextAlign = (align) => align ===  ALIGN.LEFT ? ALIGN.LEFT : ALIGN.CENTER;
-
-const getGridItemAlign = (align, layout) => {
-    const contentAlign =  getGridItemTextAlign(align);
-    if (layout === LAYOUT.HORIZONTAL) {
-        return {
-            justifyContent: contentAlign,
-        }
-    } 
-    return {
-        alignItems: contentAlign
-    }
+    return {};
+  }
+  const { color, width, style } = border;
+  return {
+    borderColor: color || "#f6f6f6",
+    borderWidth: width || "1px",
+    borderStyle: style || "solid",
+  };
 };
 
+const getLayout = (layout) => {
+  const layoutMap = {
+    [GridItemLayoutEnum.VERTICAL]: "column",
+    [GridItemLayoutEnum.HORIZONTAL]: "row",
+  };
+  return layoutMap[layout];
+};
 
-export interface GridItemProp extends TdGridItemProps, TdGridProps {}
+const getGridItemTextAlign = (align) =>
+  align === GridAlignEnum.LEFT ? GridAlignEnum.LEFT : GridAlignEnum.CENTER;
 
-const GridItem: FC<GridItemProp> = (prop) => {
-    const {
-        border = DEFAULT_BORDER, 
-        align = DEFAULT_ALIGN, 
-        column = DEFAULT_COLUMN, 
-        gutter = DEFAULT_GUTTER,
-        description,
-        image,
-        layout = DEFAULT_LAYOUT, 
-        text,
-        badgeProps,
-    } = prop;
+const getGridItemAlign = (align, layout) => {
+  const contentAlign = getGridItemTextAlign(align);
+  if (layout === GridItemLayoutEnum.HORIZONTAL) {
+    return {
+      justifyContent: contentAlign,
+    };
+  }
+  return {
+    alignItems: contentAlign,
+  };
+};
 
-    const { classPrefix } = useConfig();
+const defaultProps = {
+  border: false,
+  align: GridAlignEnum.CENTER,
+  column: 4,
+  gutter: 20,
+  description: "",
+  image: "",
+  layout: GridItemLayoutEnum.VERTICAL,
+  text: "",
+  badgeProps: null,
+};
 
-    const name = `${classPrefix}-grid-item`;
+const GridItem: FC<GridItemProp> = (props) => {
+  const {
+    border,
+    align,
+    column,
+    gutter,
+    description,
+    image,
+    layout,
+    text,
+    badgeProps,
+  } = props;
 
-    const gridItemWidth = getGridItemWidth(column);
+  const { classPrefix } = useConfig();
 
-    const gridItemBorder = getBorderStyle(border);
+  const name = `${classPrefix}-grid-item`;
 
-    const gridItemLayout = getLayout(layout);
+  const gridItemWidth = getGridItemWidth(column);
 
-    const gridItemAlign = getGridItemAlign(align, layout);
+  const gridItemBorder = getBorderStyle(border);
 
-    const gridItemTextAlign = getGridItemTextAlign(align);
+  const gridItemLayout = getLayout(layout);
 
-    const gridItemImage = useMemo(() => 
-        typeof image === 'string' ?  
-            (<img 
-                src={image} 
-                className={`${name}_image`} 
-                style={{width: '100%', height: '100%'}}
-            />) : image
-    , [image, name])
+  const gridItemAlign = getGridItemAlign(align, layout);
 
-    return <>
-        <div className={name} style={{
-            width: gridItemWidth,
-            height: gridItemWidth,
-            paddingLeft: gutter, 
-            paddingRight: gutter,
-            flexDirection: gridItemLayout,
-            textAlign: gridItemTextAlign,
-            ...gridItemAlign,
-            ...gridItemBorder
-        }}>
-            {
-                badgeProps ? <>
-                    <Badge {...badgeProps}>
-                        <div className={`${name}__image-box`}>
-                            {gridItemImage}
-                        </div>
-                    </Badge>
-                </> : <>
-                    <div className={`${name}__image-box`}>
-                        {gridItemImage}
-                    </div>
-                </>
-            }
-            <div className={`${name}__text`}>
-                <div className={`${name}__title`}>
-                    { text }
-                </div>
-                <div className={`${name}__description`}>
-                    { description }
-                </div>
-            </div>
-        </div>
-    </>
-}
+  const gridItemTextAlign = getGridItemTextAlign(align);
+
+  const gridItemImage = useMemo(
+    () =>
+      typeof image === "string" ? (
+        <img
+          src={image}
+          className={`${name}_image`}
+          style={{ width: "100%", height: "100%" }}
+        />
+      ) : (
+        image
+      ),
+    [image, name]
+  );
+
+  return withNativeProps(
+    props,
+    <div
+      className={name}
+      style={{
+        width: gridItemWidth,
+        height: gridItemWidth,
+        paddingLeft: gutter,
+        paddingRight: gutter,
+        flexDirection: gridItemLayout,
+        textAlign: gridItemTextAlign,
+        ...gridItemAlign,
+        ...gridItemBorder,
+      }}
+    >
+      {badgeProps ? (
+        <>
+          <Badge {...badgeProps}>
+            <div className={`${name}__image-box`}>{gridItemImage}</div>
+          </Badge>
+        </>
+      ) : (
+        <>
+          <div className={`${name}__image-box`}>{gridItemImage}</div>
+        </>
+      )}
+      <div className={`${name}__text`}>
+        <div className={`${name}__title`}>{text}</div>
+        <div className={`${name}__description`}>{description}</div>
+      </div>
+    </div>
+  );
+};
+
+GridItem.defaultProps = defaultProps;
+GridItem.displayName = "GridItem";
 
 export default GridItem;
